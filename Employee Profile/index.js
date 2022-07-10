ecc748a564deb1ba4236277b3c40b39d21ab098c
Mayur Shinde <70210880+mayur942@users.noreply.github.com>
2022-07-10 23:03:50 +0530

    Add files via upload

---
+++ Employee Profile/index.js
@@ -0,0 +1,29 @@
+function formTable(data) {
+    let writable_data = "<tr>";
+    let properties = [];
+    for (key in data[0]) {
+        properties.push(key);
+        writable_data += `<th> ${key}</th>`
+    }
+    // console.log(data);
+    writable_data += '</tr>'
+
+    for (let i = 0; i < data.length; i++) {
+        writable_data += "<tr>";
+        for (let j = 0; j < properties.length; j++) {
+            writable_data += `<td> ${data[i][properties[j]]}</td>`
+        }
+        writable_data += "</tr>";
+    }
+    console.log(data[0], typeof (data[0]));
+    document.getElementById("target").innerHTML = writable_data;
+}
+
+fetch("./emp.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        formTable(data);
+    });
+
