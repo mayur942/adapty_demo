d59be2cca1ba67c2d3b9d002bc63118a347e9209
Umang0001 <92432031+Umang0001@users.noreply.github.com>
2022-07-12 11:14:12 +0530

    Add files via upload

---
+++ Umang/index.js
@@ -0,0 +1,163 @@
+let Employees=
+   [
+        
+    {
+        "fname": "Umang",
+        "lname": "Bheda",
+        "gender": "M",
+        "username": "umang@gmail.com",
+        "password": "123",
+        "address": "Bhayender",
+        "previousOrg": "Infy",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Javascript",
+        "priEx": 1,
+        "secondarySkill": "React",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    
+    {
+        "fname": "Shashwat",
+        "lname": "Gupta",
+        "gender": "M",
+        "username": "shashwat@gmail.com",
+        "password": "123",
+        "address": "Chembur",
+        "previousOrg": "LTI",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Python",
+        "priEx": 2,
+        "secondarySkill": "C#",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Fravash",
+        "lname": "Mohta",
+        "gender": "M",
+        "username": "fravash@gmail.com",
+        "password": "123",
+        "address": "Thane",
+        "previousOrg": "TCS",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Java",
+        "priEx": 2,
+        "secondarySkill": "Python",
+        "secEx": 3,
+        "isAdmin": true
+    },
+    {
+        "fname": "Mayur",
+        "lname": "Shinde",
+        "gender": "M",
+        "username": "mayur@gmail.com",
+        "password": "123",
+        "address": "Nerul",
+        "previousOrg": "Atos",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "ML",
+        "priEx": 1,
+        "secondarySkill": "C++",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Nikhil",
+        "lname": "Sewani",
+        "gender": "M",
+        "username": "nikhil@gmail.com",
+        "password": "123",
+        "address": "Ulhasnagar",
+        "previousOrg": "Accenture",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Python",
+        "priEx": 1,
+        "secondarySkill": "R",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Rutuja",
+        "lname": "Bamgude",
+        "gender": "F",
+        "username": "rutuja@gmail.com",
+        "password": "123",
+        "address": "Kalyan",
+        "previousOrg": "Capgemini",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Python",
+        "priEx": 1,
+        "secondarySkill": "Java",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Bhagyashree",
+        "lname": "Salunkhe",
+        "gender": "F",
+        "username": "bhagyashree@gmail.com",
+        "password": "123",
+        "address": "Mumbai",
+        "previousOrg": "TCS",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Python",
+        "priEx": 2,
+        "secondarySkill": ".net",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Yograj",
+        "lname": "Gurav",
+        "gender": "M",
+        "username": "yograj@gmail.com",
+        "password": "456",
+        "address": "Mumbai",
+        "previousOrg": "Wipro",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Java",
+        "priEx": 1,
+        "secondarySkill": "R",
+        "secEx": 1,
+        "isAdmin": false
+    },
+    {
+        "fname": "Harsh",
+        "lname": "Raut",
+        "gender": "M",
+        "username": "harsh@gmail.com",
+        "password": "123",
+        "address": "Dhule",
+        "previousOrg": "Kotak",
+        "startDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "endDate": "Wed Jul 06 2022 15:53:04 GMT+0530 (India Standard Time)",
+        "primarySkill": "Javascript",
+        "priEx": 1,
+        "secondarySkill": "C#",
+        "secEx": 1,
+        "isAdmin": false
+    }
+]
+console.log(Employees);
+
+// console.log(register);
+
+localStorage.setItem("Employees",JSON.stringify(Employees));
+
+// let xyz=JSON.parse(localStorage.getItem("Employees"));
+// console.log(xyz ,typeof xyz);
+
+
+
+
+// console.log(document.getElementById("fname"));
+
